4f237b50a47e7a47d15ad00896952d7cfad2f51d
vahnatai <vahnatai@users.noreply.github.com>
2021-05-19 00:35:21 -0230

    linting

--- js/index.js
+++ js/index.js
@@ -2,13 +2,7 @@ import GameEngine from './GameEngine.js';
 
 window.onload = () => {
 	const gameCanvas = document.getElementById('gameCanvas');
-	// const outputBox = document.getElementById('outputBox');
-	// const inputBox = document.getElementById('inputBox');
-	// const sendButton = document.getElementById('sendButton');
-	// const pointCount = document.getElementById('pointCount');
-	// const stepCount = document.getElementById('stepCount');
 	
 	const gameEngine = new GameEngine(window, document, gameCanvas);
-	//new Game(inputBox, outputBox, pointCount, stepCount, sendButton);
 	gameEngine.start();
 };
