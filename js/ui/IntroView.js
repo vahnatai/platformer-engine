4211c87fd4a7cf4ba004c2929cdd3c5f1d19b22b
vahnatai <vahnatai@users.noreply.github.com>
2021-10-25 17:54:39 -0230

    add title to intro view

--- js/ui/IntroView.js
+++ js/ui/IntroView.js
@@ -10,7 +10,10 @@ class IntroView extends View {
 
 		this.context.lineWidth = 1;
 		this.context.strokeStyle = '#444444';
-		this.context.strokeText('INTRO SCREEN', 375, 300);
+		this.context.font = '30px sans-serif';
+		this.context.strokeText('A PLATFORMER', 300, 275);
+		this.context.font = '10px sans-serif';
+		this.context.strokeText('INTRO SCREEN', 375, 350);
 	}
 }
 
